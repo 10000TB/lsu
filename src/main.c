ae384e5698991ab4c545e4586c9fd05732c3ac18
Xuehao Hu <xuehao.hu@thoughtspot.com>
2017-08-01 15:14:23 -0700

    [Init Commit ] Initialize project base.

---
+++ src/main.c
@@ -0,0 +1,44 @@
+#include <argp.h>
+#include <stdbool.h>
+
+const char *argp_program_version = "programname programversion";
+const char *argp_program_bug_address = "<your@email.address>";
+static char doc[] = "Your program description.";
+static char args_doc[] = "[FILENAME]...";
+static struct argp_option options[] = { 
+	{ "line", 'l', 0, 0, "Compare lines instead of characters."},
+	{ "word", 'w', 0, 0, "Compare words instead of characters."},
+	{ "nocase", 'i', 0, 0, "Compare case insensitive instead of case sensitive."},
+	{ 0 } 
+};
+
+struct arguments {
+	enum { CHARACTER_MODE, WORD_MODE, LINE_MODE } mode;
+	bool isCaseInsensitive;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
+	switch (key) {
+		case 'l': arguments->mode = LINE_MODE; break;
+		case 'w': arguments->mode = WORD_MODE; break;
+		case 'i': arguments->isCaseInsensitive = true; break;
+		case ARGP_KEY_ARG: return 0;
+		default: return ARGP_ERR_UNKNOWN;
+	}   
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
+
+int main(int argc, char *argv[])
+{
+	struct arguments arguments;
+
+	arguments.mode = CHARACTER_MODE;
+	arguments.isCaseInsensitive = false;
+
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	// ...
+}
